3fe10d99e3c4f4e0b8032be626e4d46b7dbceb33
hcruzpoppinpay <126699207+hcruzpoppinpay@users.noreply.github.com>
2026-07-01 18:38:34 -0500

    feat: add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/sections/Navbar";
 import { Hero } from "@/components/sections/Hero";
 import { TrustBar } from "@/components/sections/TrustBar";
+import { About } from "@/components/sections/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <TrustBar />
+      <About />
     </>
   );
 }
